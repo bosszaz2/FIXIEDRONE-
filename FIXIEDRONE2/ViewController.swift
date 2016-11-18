ce26d0b8add0a4d5c863486d7eb87b99c01282af
bosszaz2 <jirayupromsongwong@Jirayus-MacBook-Pro.local>
2016-11-18 17:43:52 +0700

    first commit

---
+++ FIXIEDRONE2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIXIEDRONE2
+//
+//  Created by Jirayu Promsongwong on 10/19/2559 BE.
+//  Copyright © 2559 KMITL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FIXIEDRONE2/mapKitViewController.swift
@@ -0,0 +1,35 @@
+//
+//  mapKitViewController.swift
+//  FIXIEDRONE2
+//
+//  Created by Jirayu Promsongwong on 11/9/2559 BE.
+//  Copyright © 2559 KMITL. All rights reserved.
+//
+
+import UIKit
+
+class mapKitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
